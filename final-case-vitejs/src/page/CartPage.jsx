185799706b98c685bae2fad62899b1cd914af4d7
tranngocquy4 <tranngocquy1818@gmail.com>
2024-02-25 09:41:19 +0700

    1

--- final-case-vitejs/src/page/CartPage.jsx
+++ final-case-vitejs/src/page/CartPage.jsx
@@ -1,24 +1,11 @@
 import MainLayouts from "../layout/MainLayouts";
-import ProductList from "../component/product/ProductList"
+// import ProductList from "../component/product/ProductList"
 export default function CartPage ({cart}) {
     return (
       <div>
 
       <MainLayouts>
-      <h2>Giỏ hàng của bạn</h2>
-      {cart.length === 0 ? (
-        <p>Giỏ hàng của bạn đang trống</p>
-      ) : (
-        <ul>
-          {cart.map((item, index) => (
-            <li key={index}>
-              <img src={item.image} alt={item.title} style={{ width: "50px", height: "50px" }} />
-              <span>{item.title}</span>
-              <span>Price: ${item.price}</span>
-            </li>
-          ))}
-        </ul>
-      )}
+      cart
           </MainLayouts>
           </div>
     )
